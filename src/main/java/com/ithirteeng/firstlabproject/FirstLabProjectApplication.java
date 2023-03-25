08eb08204e4a5c63a0346004f0ac5292f637febe
Gulevskii Ivan <ithirteengw@gmail.com>
2023-03-25 15:01:05 +0700

    Initial commit

---
+++ src/main/java/com/ithirteeng/firstlabproject/FirstLabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ithirteeng.firstlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstLabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstLabProjectApplication.class, args);
+    }
+
+}
