0c01ef274575385a6187a70c030a1d5ef4c4c869
Gulevskii Ivan <ithirteengw@gmail.com>
2023-05-12 16:12:38 +0700

    Add mapper and some helpful functions

---
+++ chat-service/src/main/java/com/ithirteeng/messengerapi/chat/dto/chat/ChatDto.java
@@ -0,0 +1,26 @@
+package com.ithirteeng.messengerapi.chat.dto.chat;
+
+import lombok.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ChatDto {
+    private UUID id;
+
+    private Boolean isDialog;
+
+    private String chatName;
+
+    private UUID chatAdmin;
+
+    private Date creationDate;
+
+    private UUID avatarId;
+}
---
+++ chat-service/src/main/java/com/ithirteeng/messengerapi/chat/mapper/ChatMapper.java
@@ -0,0 +1,53 @@
+package com.ithirteeng.messengerapi.chat.mapper;
+
+import com.ithirteeng.messengerapi.chat.dto.chat.ChatDto;
+import com.ithirteeng.messengerapi.chat.dto.chat.CreateChatDto;
+import com.ithirteeng.messengerapi.chat.dto.chat.UpdateChatDto;
+import com.ithirteeng.messengerapi.chat.entity.ChatEntity;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class ChatMapper {
+    public static ChatEntity createChatDtoToChatEntity(CreateChatDto dto, UUID adminId) {
+        return ChatEntity.builder()
+                .chatName(dto.getChatName())
+                .chatAdmin(adminId)
+                .creationDate(new Date())
+                .id(UUID.randomUUID())
+                .isDialog(false)
+                .avatarId(dto.getAvatarId())
+                .build();
+    }
+
+    public static ChatEntity dialogueToChatEntity() {
+        return ChatEntity.builder()
+                .id(UUID.randomUUID())
+                .isDialog(true)
+                .build();
+    }
+
+    public static ChatEntity updateChatDtoToChatEntity(UpdateChatDto dto, ChatEntity entity) {
+        return ChatEntity.builder()
+                .chatName(dto.getChatName())
+                .chatAdmin(entity.getChatAdmin())
+                .creationDate(entity.getCreationDate())
+                .id(entity.getId())
+                .isDialog(entity.getIsDialog())
+                .avatarId(dto.getAvatarId())
+                .build();
+    }
+
+    public static ChatDto chatEntityToChatDto(ChatEntity entity) {
+        return ChatDto.builder()
+                .chatName(entity.getChatName())
+                .chatAdmin(entity.getChatAdmin())
+                .creationDate(entity.getCreationDate())
+                .id(entity.getId())
+                .isDialog(entity.getIsDialog())
+                .avatarId(entity.getAvatarId())
+                .build();
+    }
+
+
+}
